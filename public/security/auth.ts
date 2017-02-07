7b2ce72d2fb23fc698591e7ae9ec633539e05d1c
Joe Eames <joeeames@gmail.com>
2017-02-06 23:42:09 -0700

    updated auth.ts to use a class

--- public/security/auth.ts
+++ public/security/auth.ts
@@ -1,56 +1,65 @@
-angular.module('app').factory('auth', function($q, $http, currentIdentity) {
-  return {
-    login: function(credentials) {
-      var dfd = $q.defer();
-      $http.post('/api/login', credentials).then(function(response) {
-        currentIdentity.setUser(response.data.user);
-        
-        dfd.resolve();
-      }, function(response) {
-        dfd.reject("Invalid Credentials");
-      })
-      return dfd.promise;
-    },
-    logout: function() {
-      var dfd = $q.defer();
-      $http.post('/api/logout').then(function(response) {
-        currentIdentity.clearUser();
-        dfd.resolve();
-      }, function(response) {
-        dfd.reject("Error Logging Out");
-      })
-      return dfd.promise;
-    },
+angular.module('app').service('auth', class Auth {
+  $q: any;
+  $http: any;
+  currentIdentity: any;
+
+  constructor($q, $http, currentIdentity) {
+    this.$q = $q;
+    this.$http = $http;
+    this.currentIdentity = currentIdentity;
+  }
+  
+  login(credentials) {
+    var dfd = this.$q.defer();
+    this.$http.post('/api/login', credentials).then(response => {
+      this.currentIdentity.setUser(response.data.user);
+      
+      dfd.resolve();
+    }, response => {
+      dfd.reject("Invalid Credentials");
+    })
+    return dfd.promise;
+  }
     
-    waitForAuth: function() {
-      var dfd = $q.defer();
-      $http.get('/api/currentIdentity').then(function(response) {
-        if(!!response.data) {
-          currentIdentity.setUser(response.data);
-        }
-        dfd.resolve(currentIdentity);
-      })
-      return dfd.promise;
-    },
+  logout() {
+    var dfd = this.$q.defer();
+    this.$http.post('/api/logout').then(response => {
+      this.currentIdentity.clearUser();
+      dfd.resolve();
+    }, response => {
+      dfd.reject("Error Logging Out");
+    })
+    return dfd.promise;
+  }
     
-    requireLogin: function() {
-      return this.waitForAuth().then(function() {
-        if(currentIdentity.authenticated()) {
-          return true;
-        } else {
-          return $q.reject('AUTH_REQUIRED');
-        }
-      })
-    },
+  waitForAuth() {
+    var dfd = this.$q.defer();
+    this.$http.get('/api/currentIdentity').then(response => {
+      if(!!response.data) {
+        this.currentIdentity.setUser(response.data);
+      }
+      dfd.resolve(this.currentIdentity);
+    })
+    return dfd.promise;
+  }
     
-    requireAdmin: function() {
-      return this.waitForAuth().then(function() {
-        if(currentIdentity.authenticated() && currentIdentity.currentUser.isAdmin) {
-          return true;
-        } else {
-          return $q.reject('AUTH_REQUIRED');
-        }
-      })
-    }
+  requireLogin() {
+    return this.waitForAuth().then(() => {
+      if(this.currentIdentity.authenticated()) {
+        return true;
+      } else {
+        return this.$q.reject('AUTH_REQUIRED');
+      }
+    })
+  }
+    
+  requireAdmin() {
+    return this.waitForAuth().then(() => {
+      if(this.currentIdentity.authenticated() && this.currentIdentity.currentUser.isAdmin) {
+        return true;
+      } else {
+        return this.$q.reject('AUTH_REQUIRED');
+      }
+    })
   }
 })
