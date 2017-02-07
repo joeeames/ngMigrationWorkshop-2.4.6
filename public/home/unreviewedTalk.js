569711596fde8061540d5f3235cdc45a86660cac
Joe Eames <joeeames@gmail.com>
2017-02-06 22:04:49 -0700

    converted unreviewedTalk to a component

--- public/home/unreviewedTalk.js
+++ public/home/unreviewedTalk.js
@@ -1,20 +1,18 @@
-angular.module('app').directive('unreviewedTalk', function() {
-  return {
-    templateUrl: '/home/unreviewedTalk.html',
-    scope: {
-      session: '=',
-      voteYes: '&',
-      voteNo: '&'
-    },
-    controller: function($scope) {
-      
-      $scope.yes = function() {
-        this.voteYes();
-      }
-      
-      $scope.no = function() {
-        this.voteNo();
-      }
+angular.module('app').component('unreviewedTalk', {
+  templateUrl: '/home/unreviewedTalk.html',
+  bindings: {
+    session: '=',
+    voteYes: '&',
+    voteNo: '&'
+  },
+  controller: function() {
+    
+    this.yes = function() {
+      this.voteYes();
+    }
+    
+    this.no = function() {
+      this.voteNo();
     }
   }
 })
