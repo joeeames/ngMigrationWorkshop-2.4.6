263596669ed21e587b7e13b8c404fc7ff345b62c
Joe Eames <joeeames@gmail.com>
2017-02-06 23:49:46 -0700

    fixed ignoring js files

--- public/admin/userDetailsCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').controller('userDetailsCtrl', function (allUsers, $routeParams) {
-    this.user = allUsers.find(function (user) {
-        return user.id === parseInt($routeParams.id);
-    });
-});
-//# sourceMappingURL=userDetailsCtrl.js.map
